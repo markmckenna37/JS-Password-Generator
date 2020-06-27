60c5948445feccee5f91bf270b4ecf2c5924eca3
Mark <youremail@gmail.comg>
2020-06-27 15:33:31 -0600

    added window prompts for user input, made a while loop to insure correct user length input

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,45 @@
+  
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// var specialChar = "\"! #$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+// Establishing variables for our writePassword function, list of special characters:
+var specialChar = "\"!#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+// List of uppercase letters
+var upperCaseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// List of lowercase letters
+var lowerCaseString = "abcdefghijklmnopqrstuvwxyz";
+// List of numbers
+var numberString = "0123456789";
 // var randomSpecialChar = specialChar[Math.floor(Math.random() * specialChar.length)];
 // Write password to the #password input
 function writePassword() {
-  console.log("DOPE");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+
+  // Prompt for character length between 8-128, write a loop to make sure the user enters the correct numbers
+  function generatePassword() {  
+  var userLength = prompt("How long would you like your password to be? Type a number between 8 and 128.");
+    while (userLength < 8 || userLength > 128) {
+      alert("Please type a number between 8 and 128.");
+      userLength = prompt("How long would you like your password to be? Type a number between 8 and 128.");
+    }
+  }
+      // confirm whether the user wants upper case letters in their password
+      var userUpper = confirm("Would you like to include upper case letters in your password?");
+      // confirm whether the user wants lower case letters in their password
+      var userlower = confirm("Would you like to include lower case letters in your password?");
+      // confirm whether the user wants numbers in their password
+      var userNumber = confirm("Would you like to include numbers in your password?");
+      // confirm whether the user wants special characters in their password
+      var userSpecial = confirm("Would you like to include special characters in your password?")
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
 
 
 // function randomLower() {
@@ -26,7 +54,3 @@ generateBtn.addEventListener("click", writePassword);
 // function randomSpecial() {
 //   var computerPlay = choices[Math.floor(Math.random() * choices.length)];
 // }
-// console.log(randomLower());
-// console.log(randomUpper());
-// console.log(randomNum());
-// console.log(randomSpecialChar);
